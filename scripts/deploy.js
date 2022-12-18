82efbb741174b7001d4f736923710320c8a34d80
Aboudoc <aboutika.reda@gmail.com>
2022-12-18 04:47:09 +0100

    initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ModifyVariable = await hre.ethers.getContractFactory("ModifyVariable");
+  const modifyVariable = await ModifyVariable.deploy(10, "Alchemy");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await modifyVariable.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(`Contract deployed to ${modifyVariable.address}`);
 
-  await lock.deployed();
+  await modifyVariable.modifyLeet();
+  const newVariable = await modifyVariable.x();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Variable modified to ${newVariable}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
