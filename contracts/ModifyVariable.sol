b0a062e4ffe621143b982c987c0a172510a2bbbc
Aboudoc <aboutika.reda@gmail.com>
2022-12-18 04:37:01 +0100

    initial commit

--- contracts/ModifyVariable.sol
+++ contracts/ModifyVariable.sol
@@ -3,12 +3,18 @@ pragma solidity ^0.8.4;
 
 contract ModifyVariable {
     uint public x;
+    string public y;
 
-    constructor(uint _x) {
+    constructor(uint _x, string memory _y) {
         x = _x;
+        y = _y;
     }
 
     function modifyLeet() public {
         x = 1337;
+    }
+
+    function modifyString() public {
+        y = "Morocco";
     }
 }
--- test/simple-test.js
+++ test/simple-test.js
@@ -2,12 +2,26 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TestModifyVariable", function () {
-  it("Should modify the variable to 1337", async function () {
+  let contract;
+  beforeEach(async function () {
     const Contract = await ethers.getContractFactory("ModifyVariable");
-    const contract = await Contract.deploy(10);
+    contract = await Contract.deploy(10, "Alchemy");
     await contract.deployed();
+  });
+  it("Should modify the variable to 1337", async function () {
+    // const Contract = await ethers.getContractFactory("ModifyVariable");
+    // const contract = await Contract.deploy(10, "Alchemy");
+    // await contract.deployed();
     await contract.modifyLeet();
     const value = await contract.x();
     assert.equal(value.toNumber(), 1337);
+  });
+  it("Should modify the string", async function () {
+    // const Contract = await ethers.getContractFactory("ModifyVariable");
+    // const contract = await Contract.deploy(10, "Alchemy");
+    // await contract.deployed();
+    await contract.modifyString();
+    const value = await contract.y();
+    assert(value, "Morocco");
   });
 });
